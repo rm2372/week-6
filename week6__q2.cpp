a384fb0e84fe3a055517b92ab155b9ff00f50c47
rm2372 <104272166+rm2372@users.noreply.github.com>
2022-10-11 09:35:26 +0530

    Add files via upload

---
+++ week6__q2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class counter
+{
+int count;
+public:
+counter()
+{
+        count=0;
+}
+int get_count()
+{
+return count;
+}
+void operator++()
+{
+count++;
+}
+};
+ 
+int main()
+{
+counter c1,c2;
+cout<<"\nC1 ="<<c1.get_count();
+cout<<"\nC2 ="<<c2.get_count();
+ 
+c1++;           
+c2++;
+++c2;
+cout<<"\nC1 ="<<c1.get_count();
+cout<<"\nC2 ="<<c2.get_count();
+return 0;
+}
